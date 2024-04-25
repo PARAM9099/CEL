7409152f6237eea9c7e6c24d8792cdb206e19b1b
PARAM PATEL <158317509+PARAM9099@users.noreply.github.com>
2024-04-25 11:11:38 +0530

    Add files via upload

---
+++ Ass_2.m
@@ -0,0 +1,12 @@
+% Name - Param Patel %
+% Assignment 2 %
+% DATE - 5/02/2024 %
+% ROLL NO - 21BME115D %
+
+N = 0;
+
+for i = 1:100
+    N = N + i;
+end
+fprintf('The sum of number 1 to 100 =')
+disp(N);
---
+++ Ass_3.m
@@ -0,0 +1,16 @@
+% Name - Param Patel %
+% Assignment 3 %
+% DATE - 5/02/2024 %
+% ROLL NO - 21BME115D %
+
+x = linspace(-2*pi, 2*pi, 150);
+y = tan(x);
+
+plot(x, y, 'r', 'LineWidth', 1);
+title('Plot of tan(x)');
+xlabel('x');
+ylabel('tan(x)');
+grid on;
+
+xlim([-2*pi, 2*pi]);
+ylim([-10, 10]);
